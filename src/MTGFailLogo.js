aeb195fe87d7d6e1d955cdf8f8115feb15fd3b5a
Guy J Grigsby <guy.grigsby@gmail.com>
2020-12-03 10:28:03 -0700

    icon

--- src/MTGFailLogo.js
+++ src/MTGFailLogo.js
@@ -3,7 +3,12 @@ import { ReactComponent as Logo } from './mtgfail_white.svg'
 import PropTypes from 'prop-types'
 import { cx, css } from 'pretty-lights'
 
-const logoClass = css``
+const logoClass = css`
+  text-decoration: none;
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 export const MTGFailLogoWhite = ({ style }) => {
   return <Logo className={cx(logoClass, style)} />
