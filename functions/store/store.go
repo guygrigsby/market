50bca48ed630de328272590811688f1c27bb9fb4
Guy J Grigsby <guy.grigsby@gmail.com>
2020-12-16 12:34:37 -0700

    add Put

--- functions/store/store.go
+++ functions/store/store.go
@@ -24,10 +24,67 @@ type cardStore struct {
 	log    log15.Logger
 }
 
-func (c *cardStore) PutMany(_ map[string]*mtgfail.Entry, _ log15.Logger) error {
+func (c *cardStore) PutMany(cards map[string]*mtgfail.Entry, log log15.Logger) error {
+
+	coll := c.client.Collection(collection)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	results := make(chan *firestore.WriteResult)
+	errs := make(chan error)
+	var wg sync.WaitGroup
+
+	for k, v := range cards {
+		wg.Add(1)
+		name, card := k, v
+		go func() {
+			defer wg.Done()
+			name = NormalizeCardName(name, log)
+			doc := coll.Doc(name)
+			wr, err := doc.Set(ctx, card)
+			if err != nil {
+				c.log.Error(
+					"Cannot Set Data in store.",
+					"err", err,
+					"name", name,
+				)
+				errs <- err
+				return
+			}
+			results <- wr
+		}()
+	}
+
+	for range cards {
+		select {
+		case <-results:
+		case err := <-errs:
+			c.log.Error(
+				"Error in write. Canceling",
+				"err", err,
+			)
+			return err
+		}
+	}
+
 	return nil
 }
-func (c *cardStore) Put(_ string, _ *mtgfail.Entry, _ log15.Logger) error {
+func (c *cardStore) Put(key string, card *mtgfail.Entry, log log15.Logger) error {
+
+	coll := c.client.Collection(collection)
+	ctx := context.Background()
+
+	name := NormalizeCardName(card.Name, log)
+	doc := coll.Doc(name)
+	_, err := doc.Set(ctx, card)
+	if err != nil {
+		c.log.Error(
+			"Cannot Get Data from store",
+			"err", err,
+			"name", name,
+		)
+		return err
+	}
+
 	return nil
 }
 
