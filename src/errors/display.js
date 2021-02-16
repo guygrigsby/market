ea0804e6b20a79d142e220659b618c6325c93739
Guy J Grigsby <guy.grigsby@gmail.com>
2021-02-15 23:36:33 -0700

    add errors page

---
+++ src/errors/display.js
@@ -0,0 +1,10 @@
+const matcher = new RegExp(
+  `failed to build deck rpc error: code = NotFound desc = "projects\/marketplace-c87d0\/databases\/\(default\)\/documents\/cards\/(.+)" not found`,
+)
+
+export const decodeResponse = (res) => {
+  console.log(res)
+  const m = res.match(matcher)
+  console.log(m)
+  return m && m.length > 0 ? `Failed to build deck: ${m[1]}` : res
+}
